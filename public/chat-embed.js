0dd8ec78335523d53beee24e0092f924d1481e2e
Xavi Castello <info@xaviercastello.com>
2026-05-18 23:36:01 +0200

    fix: botó tancar mòbil sobre el camp d'escriure

--- public/chat-embed.js
+++ public/chat-embed.js
@@ -53,14 +53,14 @@
       container.style.display = isOpen ? 'block' : 'none';
 
       if (isOpen) {
-        btn.style.bottom = 'auto';
-        btn.style.top = '12px';
+        btn.style.top = 'auto';
+        btn.style.bottom = '80px';
         btn.style.right = '12px';
         btn.style.height = '40px';
         btn.style.padding = '0 14px';
         btn.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="white" style="flex-shrink:0"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
-          <span style="font-family:sans-serif; font-size:13px; font-weight:600; color:white;">Tancar</span>`;
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="white" style="flex-shrink:0"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
+    <span style="font-family:sans-serif; font-size:13px; font-weight:600; color:white;">Tancar</span>`;
       } else {
         btn.style.top = 'auto';
         btn.style.bottom = '24px';
